0048d0a9064ea28a7dab887d8ca981ffbe5115ff
stevo <spolin44@gmail.com>
2023-09-11 13:35:37 -0400

    created manager

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.0
 
 require gitlab.com/tesla-scripts/go-utils v0.0.0-20230905194905-341e819fe02a
 
-require github.com/BurntSushi/toml v1.3.2 // indirect
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/stevohuncho/GoFile v1.0.0 // indirect
+)
--- manager.go
+++ manager.go
@@ -1,9 +1,11 @@
 package dirman
 
 import (
+	"fmt"
 	"io/fs"
+	"path/filepath"
 
-	"gitlab.com/tesla-scripts/go-utils/fastfile"
+	gofile "github.com/stevohuncho/GoFile"
 )
 
 type DirMan struct {
@@ -12,45 +14,76 @@ type DirMan struct {
 
 // creates manager and saves the path as the root dir
 func NewDirManager(path string) (*DirMan, error) {
-	return nil, nil
+	if !gofile.Exists(path) {
+		return nil, fmt.Errorf("failed to find path: %s", path)
+	}
+	dm := DirMan{
+		root: path,
+	}
+	return &dm, nil
 }
 
 // makes new dir
 func (dm *DirMan) Mkdir(path string) error {
-	return nil
+	newPath := filepath.Join(dm.root, path)
+	return gofile.SafeDir(newPath)
 }
 
 // removes file/dir
 func (dm *DirMan) Rm(path string) error {
-	return nil
+	newPath := filepath.Join(dm.root, path)
+	return gofile.Rm(newPath)
+}
+
+// removes file/dir
+func (dm *DirMan) RmAll(path string) error {
+	newPath := filepath.Join(dm.root, path)
+	return gofile.RmAll(newPath)
 }
 
 // writes data to path
-func (dm *DirMan) WriteJson(path string, data any) error {
-	return nil
+func (dm *DirMan) WriteJson(path string, d any) error {
+	newPath := filepath.Join(dm.root, path)
+	return gofile.WriteJson(newPath, d)
+}
+
+func (dm *DirMan) WriteIndentedJson(path string, d any) error {
+	newPath := filepath.Join(dm.root, path)
+	return gofile.WriteIndentedJson(newPath, d)
 }
 
 // retrieves all files/dirs filtered by fastfile filters
-func (dm *DirMan) Ls(path string, opts ...fastfile.ReadDirOptFunc) ([]fs.DirEntry, error) {
-	return []fs.DirEntry{}, nil
+func (dm *DirMan) Ls(path string, opts ...gofile.ReadDirOptFunc) ([]fs.DirEntry, error) {
+	newPath := filepath.Join(dm.root, path)
+	return gofile.ReadDir(newPath, opts...)
 }
 
 // gets the string data of the file
 func (dm *DirMan) String(path string) (string, error) {
-	return "", nil
+	newPath := filepath.Join(dm.root, path)
+	return gofile.String(newPath)
 }
 
 // gets json data of the file and saves it to the pointer
-func (dm *DirMan) Json(path string, pointer interface{}) error {
-	return nil
+func (dm *DirMan) Json(path string, p interface{}) error {
+	newPath := filepath.Join(dm.root, path)
+	return gofile.Json(newPath, p)
 }
 
 // gets the csv data of the file
 func (dm *DirMan) Csv(path string) ([][]string, error) {
-	return [][]string{}, nil
+	newPath := filepath.Join(dm.root, path)
+	return gofile.Csv(newPath)
 }
 
 // creates a new manager relative to the root of the current
 func (dm *DirMan) New(path string) (*DirMan, error) {
-	return nil, nil
+	newPath := filepath.Join(dm.root, path)
+	if !gofile.Exists(newPath) {
+		return nil, fmt.Errorf("failed to find path: %s", newPath)
+	}
+	newDm := DirMan{
+		root: newPath,
+	}
+	return &newDm, nil
 }
